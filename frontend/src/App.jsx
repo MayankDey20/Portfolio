71d7331e6c3fec909041583f5aa9d90aa28e11af
Mayank Dey <mayank@Mayanks-MacBook-Air.local>
2026-04-05 21:56:31 +0530

    feat: implement 3D interactive Atmospheric Globe with real-time weather and location popups

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import TechStack from './components/TechStack'
 import SplineRobot from './components/SplineRobot'
 import ProjectCards from './components/ProjectCards'
 import ContactSection from './components/ContactSection'
+import AtmosphericGlobe from './components/AtmosphericGlobe'
 import Footer from './components/Footer'
 
 import SmoothScroll from './components/SmoothScroll'
@@ -40,6 +41,9 @@ function App() {
         
         {/* Contact Gateway */}
         <ContactSection />
+        
+        {/* Dynamic Atmospheric Globe CTA */}
+        <AtmosphericGlobe />
         
         {/* Site Footer */}
         <Footer />
